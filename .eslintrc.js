3215ff93145fecb1201b78e6fc0a7daca79fdc3f
Tobias Ortmayr <tortmayr@eclipsesource.com>
2022-07-27 05:37:56 -0700

    GH-708 Improve eslint config (#190)
    - Refactor eslint config according to the recommendations of https://typescript-eslint.io/docs/linting/monorepo/
    - Add CLI script for fixing eslint issues
    
    Part of https://github.com/eclipse-glsp/glsp/issues/708

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,19 @@ module.exports = {
     parserOptions: {
         tsconfigRootDir: __dirname,
         project: 'tsconfig.eslint.json'
+    },
+    rules: {
+        'no-restricted-imports': [
+            'warn',
+            {
+                name: 'sprotty',
+                message: "The sprotty default exports are customized and reexported by GLSP. Please use '@eclipse-glsp/client' instead"
+            },
+            {
+                name: 'sprotty-protocol',
+                message:
+                    "The sprotty-protocol default exports are customized and reexported by GLSP. Please use '@eclipse-glsp/client' instead"
+            }
+        ]
     }
 };
